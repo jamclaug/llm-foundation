7c87918d41d233af54f6127902ef7b3e00adaf7d
jamclaug <jamie.mclaughlin@gmail.com>
2025-11-25 09:13:58 +0100

    Implemented backprop, inspired by micrograd, as I did this while watchign the video by Andrej Karpathy.

--- phase1_foundation/backprop_from_scratch.py
+++ phase1_foundation/backprop_from_scratch.py
@@ -9,21 +9,69 @@ class Value:
         self._prev = set(children) # set of child nodes
         self._op = _op # operation that produced this node
         self.label = label # optional label for visualization
+        self._backward = lambda: None
 
     def __repr__(self):
         return f"Value(data={self.data})"
     
     def __add__(self, other):
         out = Value(self.data + other.data, (self, other), '+')
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
         return out
     
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), '*')
+        def _backward():
+            self.grad += other.data*out.grad
+            other.grad += self.data*out.grad
+        out._backward = _backward
         return out
     
+    def __rmul__(self, other):
+        return self * other
+    
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+
+        def _backward():
+            local_deriv = 1.0 if out.data == 0 else out.data
+            self.grad += local_deriv * out.grad
+        out._backward = _backward
+        return out
+
+    def __truediv__(self, other):
+        return self * other**-1
+
     def tanh(self):
         n = self.data
         tanh = (math.exp(2*n)-1)/(math.exp(2*n)+1)
         out = Value(tanh, (self,), 'tanh')
+        def _backward():
+            local_deriv = (1-tanh**2)
+            self.grad = local_deriv * out.grad
+        out._backward = _backward
         return out
     
+    def backward(self):
+        # Build the list of nodes in forward order
+        nodes = []
+        visited = set()
+        def build_nodes(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_nodes(child)
+                nodes.append(v)
+
+        build_nodes(self)        
+        # We need to traverse backwards through all the nodes of the expression, so reverse the list
+        nodes = list(reversed(nodes))
+        
+        # Initialize the gradient
+        self.grad = 1.0
+        for node in nodes:
+            node._backward()
